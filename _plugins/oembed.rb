30797259c11df6044bf1d0cdce30a219993c3adf
“xjmzx” <tjmzxuk@gmail.com>
2020-08-01 20:46:27 +0700

    Update oembed.rb

--- _plugins/oembed.rb
+++ _plugins/oembed.rb
@@ -3,22 +3,12 @@
 #   - requires https://github.com/judofyr/ruby-oembed/
 #
 # Copyright 2011 Tammo van Lessen
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
 #        http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
 ##
 require 'oembed'
 require 'uri'
-
+require 'openssl'
 # register all default OEmbed providers
 ::OEmbed::Providers.register_all()
 # since register_all does not register all default providers, we need to do this here. See https://github.com/judofyr/ruby-oembed/issues/18
